0ba07ce46e5b3c732cafcbeb765708a551870ee4
Aitor Salvador <aitorsg93@gmail.com>
2022-02-09 22:42:57 +0100

    Creates EMS module

---
+++ Modules/Domain/EMSDomain/Example/EMSDomain/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  EMSDomain
+//
+//  Created by llenyador on 02/09/2022.
+//  Copyright (c) 2022 llenyador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
